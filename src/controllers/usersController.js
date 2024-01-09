249302e801a13d2612ba61654bd4c984c5e3c082
DuongLinh <nguyenduonglinh9@gmail.com>
2024-01-09 10:54:16 +0700

    chỉnh sửa lỗi

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -13,71 +13,42 @@ const userController = {
       .catch((err) => next(err));
   },
 
-  createOne: async (req, res, next) => {
-    const checkUser = await User.find({ email: req.body.email });
-    if (checkUser) {
-      res.json({ code: 400, message: "Email already exists !" });
+  findUsers: async (req, res, next) => {
+    const keywords = new RegExp(req.query.keywords);
+
+    const filterName = await User.find({
+      fullname: { $regex: keywords, $options: "i" },
+    });
+    if (filterName.length > 0) {
+      res.json({
+        code: 200,
+        message: filterName,
+      });
     } else {
-      try {
-        const newUser = new User({
-          fullname: req.body.fullname,
-          email: req.body.email,
-          createdAt: req.body.createdAt,
-          imageURL: req.body.imageURL,
-          address: req.body.address,
-          phone: req.body.phone,
-          files: req.body.files,
-        });
-        await newUser.save();
+      const filterEmail = await User.find({
+        email: { $regex: keywords, $options: "i" },
+      }).exec();
+      if (filterEmail.length > 0) {
         res.json({
           code: 200,
-          message: "success",
+          message: filterEmail,
         });
-      } catch (error) {
-        res.json({
-          code: 400,
-          message: error,
-        });
-      }
-    }
-  },
-
-  getOne: async (req, res, next) => {
-    await User.findOne({ googleID: req.params.id })
-      .then((user) => {
-        res.json(user);
-      })
-      .catch((err) => next(err));
-  },
-  getStaff: async (req, res, next) => {
-    await User.find({ role: "staff" })
-      .then((staffs) => res.json(staffs))
-      .catch((err) => next(err));
-  },
-  getAdmin: async (req, res, next) => {
-    await User.find({ role: "admin" })
-      .then((admins) => res.json(admins))
-      .catch((err) => next(err));
-  },
-  updateUser: async (req, res, next) => {
-    try {
-      const newUser = await User.findOneAndUpdate(
-        { googleID: req.params.id },
-        {
-          role: req.body.role,
-          employeeType: req.body.employeeType,
-        },
-        {
-          new: true,
-        }
-      );
-      if (newUser) {
-        res.json({ code: 200, message: newUser });
       } else {
-        res.json({ code: 400, message: "error" });
+        const filterPhone = await User.find({
+          phone: { $regex: keywords, $options: "i" },
+        }).exec();
+        if (filterPhone.length > 0) {
+          res.json({
+            code: 200,
+            message: filterPhone,
+          });
+        } else {
+          res.json({
+            code: 404,
+            message: "Not Found",
+          });
+        }
       }
-    } catch (error) {
-      res.json({ code: 400, message: "LỖI : " + error });
     }
   },
 };
